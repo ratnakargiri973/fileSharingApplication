651d04cd8004fec3ade825d717dfec9c8c59c401
Ratnakar <ratnakargiri973@gmail.com>
2024-12-15 00:31:56 +0530

    message added

--- Client/Frontend/src/Components/uploadFile.jsx
+++ Client/Frontend/src/Components/uploadFile.jsx
@@ -3,6 +3,7 @@ import axios from 'axios';
 
 function uploadFile() {
     const [files, setFiles] = useState(null);
+    const [message, setMessage] = useState("");
 
     function handleChange(e){
         setFiles(e.target.files[0]);
@@ -15,6 +16,7 @@ function uploadFile() {
          data.append("file", files);
          setFiles(null);
          const response = await axios.post('https://filesharingapplication-server.onrender.com/api/file/upload', data);
+         setMessage(response.data.message);
          console.log(response.data);
        } 
        catch (error) {
@@ -24,14 +26,19 @@ function uploadFile() {
   return (
     <div className='flex justify-center items-center gap-4 flex-col'>
       <h1 className='font-bold text-3xl text-teal-700'>Upload Files</h1>
-      <form action="" encType="multipart/form-data" onSubmit={handleSubmit}>
-      <input 
-      type="file"
-      name='file'
-      onChange={handleChange} 
-      className='outline-none rounded border-none'/>
-      <button type='submit' className='p-2 rounded bg-blue-400 hover:bg-blue-600 hover:text-white'>Upload</button>
-      </form>
+      {message.length > 0 ? (
+        <h4 className='font-bold text-3xl text-teal-700'>{message}</h4>
+      ) : (
+        <form action="" encType="multipart/form-data" onSubmit={handleSubmit}>
+        <input 
+        type="file"
+        name='file'
+        onChange={handleChange} 
+        className='outline-none rounded border-none'/>
+        <button type='submit' className='p-2 rounded bg-blue-400 hover:bg-blue-600 hover:text-white'>Upload</button>
+        </form>
+      )}
+     
     </div>
   )
 }
