10397b55329bdc17a3cc064102eae77482de1694
Ratnakar <ratnakargiri973@gmail.com>
2024-12-15 00:33:43 +0530

    listfile modified

--- Client/Frontend/src/Components/ListFiles.jsx
+++ Client/Frontend/src/Components/ListFiles.jsx
@@ -4,7 +4,7 @@ function ListFiles({files}) {
   return (
     <div className='flex justify-center items-center gap-4 flex-col'>
       <h3 className='font-bold text-3xl text-teal-700'>Uploaded Files</h3>
-      <div className='flex gap-2 '>
+      <div className='flex gap-2 flex-wrap'>
       {files.map((file) => {
         return(
             <li key={file.uuid} className='list-none w-44 h-44 shadow-md shadow-green-400 bg-emerald-300 rounded text-center flex items-center'>
